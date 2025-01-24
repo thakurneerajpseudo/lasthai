1dfcfdd5eee74344160d749bbfc3637cbb3b67a3
Admin <Admin@DESKTOP-636FH5J>
2025-01-25 00:03:22 +0530

    sum method added

--- src/myDummY/dekhBhae.java
+++ src/myDummY/dekhBhae.java
@@ -3,5 +3,11 @@ package myDummY;
 public class dekhBhae {
 public static void main(String[] args) {
 	System.out.println("Ae dil hai mushkeel");
+	System.out.println(sum(12, 4));
+}
+
+
+static int sum(int a ,int b) {
+	return a+b;
 }
 }
